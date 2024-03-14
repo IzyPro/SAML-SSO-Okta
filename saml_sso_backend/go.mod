45c3fd3cec5fe670dd35b9abcb07fb4b0c1bc2fc
Israel Ulelu <45710398+IzyPro@users.noreply.github.com>
2024-03-14 10:50:54 +0100

    ux improvements and addition of env variables

--- saml_sso_backend/go.mod
+++ saml_sso_backend/go.mod
@@ -7,5 +7,6 @@ require github.com/stytchauth/stytch-go/v12 v12.2.1
 require (
 	github.com/MicahParks/keyfunc/v2 v2.0.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 )
--- saml_sso_backend/main.go
+++ saml_sso_backend/main.go
@@ -6,22 +6,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/stytchauth/stytch-go/v12/stytch/b2b/b2bstytchapi"
 	"github.com/stytchauth/stytch-go/v12/stytch/b2b/sso"
 )
 
-const PROJECT_ID = "YOUR_STYTCH_PROJECT_ID"
-const SECRET_KEY = "YOUR_STYTCH_SECRET_KEY"
 const PORT = ":3001"
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	http.HandleFunc("/authenticate", authenticate)
 	fmt.Println("Server is listening on port", PORT)
 	log.Fatal(http.ListenAndServe(PORT, nil))
 }
 
 func authenticate(w http.ResponseWriter, r *http.Request) {
+	PROJECT_ID := os.Getenv("STYTCH_PROJECT_ID")
+	SECRET_KEY := os.Getenv("STYTCH_SECRET_KEY")
+
 	token := r.URL.Query().Get("token")
 	if token == "" {
 		httpError(w, "Failed to authenticate user. Token is null")
@@ -44,10 +52,8 @@ func authenticate(w http.ResponseWriter, r *http.Request) {
 		httpError(w, err.Error())
 		return
 	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	message := fmt.Sprintf("<h3>SAML SSO Login Process Complete!</h3><br/><p>You  have been successfully Logged in to the %s organization. Your role is %s and your session expires at %s.</p><br/><br/>Session Token: %s<br/>Session JWT: %s", resp.Organization.OrganizationName, resp.MemberSession.Roles[0], resp.MemberSession.ExpiresAt, resp.SessionToken, resp.SessionJWT)
-	fmt.Fprint(w, message)
+	http.Redirect(w, r, "http://localhost:3000/success", http.StatusSeeOther)
+	fmt.Println(resp.StatusCode)
 }
 
 func httpError(w http.ResponseWriter, message string) {
